f037ad9605a385aea8eba1af352b8a99cb01cdca
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-09-07 00:08:37 +0300

    feat: create header-navigation component, minor fixes in header-info

--- shop/src/app/core/core.module.ts
+++ shop/src/app/core/core.module.ts
@@ -3,15 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { HeaderInfoComponent } from './header/header-info/header-info.component';
+import { HeaderNavigationComponent } from './header/header-nav/header-navigation.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HeaderComponent, HeaderInfoComponent],
-  imports: [CommonModule, MatButtonModule, MatMenuModule, MatDividerModule, CoreRoutingModule],
+  declarations: [HeaderComponent, HeaderInfoComponent, HeaderNavigationComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    CoreRoutingModule,
+  ],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
