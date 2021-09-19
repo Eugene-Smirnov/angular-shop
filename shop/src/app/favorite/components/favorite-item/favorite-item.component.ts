60e39135c3a0d0c8aa2dc0f8241cfd310468ea9b
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-09-19 18:09:46 +0300

    feat: implement adding and removing items to cart from favorite-page

--- shop/src/app/favorite/components/favorite-item/favorite-item.component.ts
+++ shop/src/app/favorite/components/favorite-item/favorite-item.component.ts
@@ -1,7 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { take } from 'rxjs/operators';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { GoodsItemModel } from 'src/app/goods/models/goods-item.model';
-import { GoodsService } from 'src/app/goods/services/goods.service';
 import { ratingToStars } from 'src/app/shared/variables';
 
 @Component({
@@ -9,23 +7,16 @@ import { ratingToStars } from 'src/app/shared/variables';
   templateUrl: './favorite-item.component.html',
   styleUrls: ['./favorite-item.component.scss'],
 })
-export class FavoriteItemComponent implements OnInit {
-  @Input() itemId: string = '';
+export class FavoriteItemComponent {
+  @Input() item: GoodsItemModel | null = null;
 
   @Output() removeClick: EventEmitter<string> = new EventEmitter();
 
+  @Output() toCartClick: EventEmitter<string> = new EventEmitter();
+
+  @Output() removeFromCartClick: EventEmitter<string> = new EventEmitter();
+
   @Output() linkClick: EventEmitter<string> = new EventEmitter();
 
-  constructor(private goodsService: GoodsService) {}
-
   ratingToStars = ratingToStars;
-
-  item: GoodsItemModel | null = null;
-
-  ngOnInit(): void {
-    this.goodsService
-      .getByItemId(this.itemId)
-      .pipe(take(1))
-      .subscribe((item) => (this.item = item));
-  }
 }
--- shop/src/app/favorite/pages/favorite-page/favorite-page.component.ts
+++ shop/src/app/favorite/pages/favorite-page/favorite-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { forkJoin, of, Subscription } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { GoodsItemModel } from 'src/app/goods/models/goods-item.model';
+import { GoodsService } from 'src/app/goods/services/goods.service';
 import { loadUserInfo } from 'src/app/redux/actions/user.actions';
-import { selectFavorites } from 'src/app/redux/selectors/user.selector';
+import { selectCart, selectFavorites } from 'src/app/redux/selectors/user.selector';
 import { UserService } from 'src/app/user/services/user.service';
 
 @Component({
@@ -12,11 +15,18 @@ import { UserService } from 'src/app/user/services/user.service';
   styleUrls: ['./favorite-page.component.scss'],
 })
 export class FavoritePageComponent implements OnInit {
-  constructor(private store: Store, private userService: UserService, private router: Router) {}
+  constructor(
+    private store: Store,
+    private userService: UserService,
+    private router: Router,
+    private goodsService: GoodsService,
+  ) {}
 
   favorites$ = this.store.select(selectFavorites);
 
-  favorites: string[] | null = [];
+  cart$ = this.store.select(selectCart);
+
+  favorites: GoodsItemModel[] = [];
 
   subscriptions = new Subscription();
 
@@ -24,19 +34,49 @@ export class FavoritePageComponent implements OnInit {
 
   ngOnInit(): void {
     this.subscriptions.add(
-      this.favorites$.subscribe((favorites) => {
-        if (!favorites || !favorites[0]) {
-          this.favorites = [];
-          return;
-        }
-        this.favorites = favorites;
-        this.cols = Math.ceil(this.favorites.length / 2);
-      }),
+      this.favorites$
+        .pipe(
+          switchMap((favorites) => {
+            if (!favorites || !favorites[0]) return of(null);
+            return forkJoin(favorites.map((itemId) => this.goodsService.getByItemId(itemId)));
+          }),
+          switchMap((favorites) => {
+            if (!favorites || !favorites[0]) return of(null);
+            return this.cart$.pipe(
+              map((cart) => {
+                if (!cart || !cart[0]) return favorites;
+                return favorites.map((item) => {
+                  return { ...item, isInCart: cart.includes(item.id) };
+                });
+              }),
+            );
+          }),
+        )
+        .subscribe((favorites) => {
+          if (!favorites || !favorites[0]) {
+            this.favorites = [];
+            return;
+          }
+          this.favorites = favorites;
+          this.cols = Math.ceil(this.favorites.length / 2);
+        }),
     );
   }
 
   onRemoveClick(itemId: string): void {
     this.userService.deleteFavorite(itemId).subscribe(() => {
+      this.store.dispatch(loadUserInfo());
+    });
+  }
+
+  onToCartClick(itemId: string): void {
+    this.userService.addToCart(itemId).subscribe(() => {
+      this.store.dispatch(loadUserInfo());
+    });
+  }
+
+  onRemoveFromCartClick(itemId: string): void {
+    this.userService.deleteFromCart(itemId).subscribe(() => {
       this.store.dispatch(loadUserInfo());
     });
   }
